e67f8c1b8288078f6f46c6754be297d8071a1913
ArgsEngineer <ryo.args.engineer@gmail.com>
2020-12-08 01:35:38 +0900

    ページトップへのスクロールボタンを追加（全画面共通）

---
+++ core/src/components/Animation/ScrollToTop.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { css } from '@emotion/core';
+
+export const ScrollToTop: React.FC = () => {
+  const [isTop, setIsTop] = useState<boolean>(true);
+
+  const onScroll = (): void => {
+    if (getTop() >= 100) {
+      setIsTop(false);
+    } else {
+      setIsTop(true);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', onScroll);
+    return (): void => document.removeEventListener('scroll', onScroll);
+  });
+
+  return <div css={[ScrollToTopStyles, isTop ? HideScrollToTop : '']} onClick={scroll} />;
+};
+
+const getTop = (): number =>
+  Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop);
+
+const scroll = (): void => {
+  typeof window !== 'undefined' &&
+    window.scroll({
+      top: 0, // 最上部へスクロール
+      behavior: 'smooth',
+    });
+};
+
+const ScrollToTopStyles = css`
+  background-image: url(${require('../../content/img/ScrollToTop.png')});
+  cursor: pointer;
+  position: fixed;
+  right: 5%;
+  bottom: 5%;
+  min-width: 48px;
+  min-height: 48px;
+  z-index: 100;
+  transition: opacity 1s, visibility 1s, transform 0.5s;
+  -webkit-transition: opacity 1s, visibility 1s, -webkit-transform 0.5s;
+  opacity: 0.5;
+  visibility: visible;
+
+  /* PCのみ */
+  @media (min-width: 795px) and (min-height: 795px) {
+    :hover {
+      transform: scale(1.3);
+      -webkit-transform: scale(1.3);
+    }
+  }
+`;
+
+const HideScrollToTop = css`
+  z-index: 0;
+  opacity: 0;
+  visibility: hidden;
+`;
--- core/src/components/Wrapper.tsx
+++ core/src/components/Wrapper.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { ScrollToTop } from '../components/Animation/ScrollToTop';
 
 interface WrapperProps {
   className?: string;
 }
 
 export const Wrapper: React.FC<WrapperProps> = ({ children, className }) => (
-  <StyledWrapper className={className}>{children}</StyledWrapper>
+  <StyledWrapper className={className}>
+    {children}
+    <ScrollToTop />
+  </StyledWrapper>
 );
 
 const StyledWrapper = styled.div`
@@ -14,4 +18,3 @@ const StyledWrapper = styled.div`
   flex-direction: column;
   min-height: 100vh;
 `;
-
